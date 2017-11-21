6ac0723e6938c8b3724e853a45ab419a4561992d
Eric Tieniber <tieniber@gmail.com>
2017-11-21 12:56:30 -0500

    added single/multi option

--- src/ReferenceSetMultiSelect/widget/ReferenceSetMultiSelect.js
+++ src/ReferenceSetMultiSelect/widget/ReferenceSetMultiSelect.js
@@ -1,3 +1,4 @@
+/*globals define */
 define([
     "dojo/_base/declare",
     "mxui/widget/_WidgetBase",
@@ -16,10 +17,10 @@ define([
     "dojo/_base/event",
 
     "dojo/text!ReferenceSetMultiSelect/widget/template/ReferenceSetMultiSelect.html"
-], function (declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, dojoLang, dojoText, dojoHtml, dojoEvent, widgetTemplate) {
+], function(declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, dojoLang, dojoText, dojoHtml, dojoEvent, widgetTemplate) {
     "use strict";
 
-    return declare("ReferenceSetMultiSelect.widget.ReferenceSetMultiSelect", [ _WidgetBase, _TemplatedMixin ], {
+    return declare("ReferenceSetMultiSelect.widget.ReferenceSetMultiSelect", [_WidgetBase, _TemplatedMixin], {
 
         templateString: widgetTemplate,
 
@@ -29,143 +30,153 @@ define([
         // Internal variables.
         _handles: null,
         _contextObj: null,
-		_referenceSetToChange: null,
-		_referenceSetToListen: null,
-		_attrHandle: null,
+        _referenceSetToChange: null,
+        _referenceSetToListen: null,
+        _attrHandle: null,
 
-        constructor: function () {
+        constructor: function() {
             this._handles = [];
         },
 
-        postCreate: function () {
+        postCreate: function() {
             logger.debug(this.id + ".postCreate");
-			this.connect(this.selectNode, "change", this._selectionChanged);
+            this.connect(this.selectNode, "change", this._selectionChanged);
 
-			this._referenceSetToChange = this.associationToSet.split("/")[0];
-			this._referenceSetToListen = this.pathToListen.split("/")[0];
+			//set single or multi-select
+			var att;
+			if(this.multiselect) {
+				att = document.createAttribute("multiple");
+				this.selectNode.setAttributeNode(att);
+			} else {
+				att = document.createAttribute("size");
+				att.value = "4";
+				this.selectNode.setAttributeNode(att);
+			}
+
+            this._referenceSetToChange = this.associationToSet.split("/")[0];
+            this._referenceSetToListen = this.pathToListen.split("/")[0];
         },
 
-        update: function (obj, callback) {
+        update: function(obj, callback) {
             logger.debug(this.id + ".update");
 
             this._contextObj = obj;
             this._updateRendering(callback);
-			this._resetSubscriptions();
+            this._resetSubscriptions();
         },
 
-        resize: function (box) {
-          logger.debug(this.id + ".resize");
+        resize: function(box) {
+            logger.debug(this.id + ".resize");
         },
 
-        uninitialize: function () {
-          logger.debug(this.id + ".uninitialize");
+        uninitialize: function() {
+            logger.debug(this.id + ".uninitialize");
         },
 
-        _updateRendering: function (callback) {
+        _updateRendering: function(callback) {
             logger.debug(this.id + "._updateRendering");
-			//empty the node
-			this.selectNode.innerHTML = "";
-			this._selectionChanged();
+            //empty the node
+            this.selectNode.innerHTML = "";
+            this._selectionChanged();
 
-			//and refill it
+            //and refill it
             if (this.useConstraint) {
-				//constrained path
-				var splitPath = this.pathToListen.split("/");
-				mx.data.get({
-					guid: this._contextObj.getGuid(),
-					path: splitPath[0],
-					callback: function(objs) {
-						dojoLang.hitch(this, this._getListObjects, objs)();
-					}
-				}, this);
-			} else {
-				//just xpath
-				mx.data.get({
-					xpath:"//" + this.listEntity,
-					callback: function(objs) {
-						dojoLang.hitch(this, this._setupOptions, objs)();
-					}
-				}, this);
-			}
-			if(callback) callback();
+                //constrained path
+                var splitPath = this.pathToListen.split("/");
+                mx.data.get({
+                    guid: this._contextObj.getGuid(),
+                    path: splitPath[0],
+                    callback: function(objs) {
+                        dojoLang.hitch(this, this._getListObjects, objs)();
+                    }
+                }, this);
+            } else {
+                //just xpath
+                mx.data.get({
+                    xpath: "//" + this.listEntity,
+                    callback: function(objs) {
+                        dojoLang.hitch(this, this._setupOptions, objs)();
+                    }
+                }, this);
+            }
+            if (callback) callback();
         },
-		_getListObjects: function(objs) {
-			var curObj, splitPath;
-			for (var i=0; i<objs.length; i++) {
-				curObj = objs[i];
-				splitPath = this.pathFromListener.split("/");
-				mx.data.get({
-					guid: curObj.getGuid(),
-					path: splitPath[0],
-					callback: function(objs2) {
-						dojoLang.hitch(this, this._setupOptions, objs2)();
-					}
-				}, this);
-			}
-		},
-		_setupOptions: function(objs) {
-			var tempOption, curObj, objLabel, objValue;
-			for (var i=0; i<objs.length; i++) {
-				curObj = objs[i];
-				objLabel = curObj.get(this.labelAttribute);
-				objValue = curObj.getGuid();
-				tempOption = dojoConstruct.toDom("<option value='" + objValue + "'>" + objLabel + "</option>")
-				this.selectNode.appendChild(tempOption);
-			}
-		},
-		_selectionChanged: function() {
-			this._contextObj.set(this._referenceSetToChange, this._getSelectValues(this.selectNode));
-			if(this.onChangemf) {
-				mx.data.action({
-				    params: {
-				        applyto: "selection",
-				        actionname: this.onChangemf,
-				        guids: [this._contextObj.getGuid()]
-				    },
-				    origin: this.mxform,
-				    callback: function(obj) {
-				    },
-				    error: function(error) {
-				        alert(error.message);
-				    }
-				});
-			}
-		},
-		_getSelectValues: function(select) {
-			var result = [];
-			var options = select && select.options;
-			var opt;
+        _getListObjects: function(objs) {
+            var curObj, splitPath;
+            for (var i = 0; i < objs.length; i++) {
+                curObj = objs[i];
+                splitPath = this.pathFromListener.split("/");
+                mx.data.get({
+                    guid: curObj.getGuid(),
+                    path: splitPath[0],
+                    callback: function(objs2) {
+                        dojoLang.hitch(this, this._setupOptions, objs2)();
+                    }
+                }, this);
+            }
+        },
+        _setupOptions: function(objs) {
+            var tempOption, curObj, objLabel, objValue;
+            for (var i = 0; i < objs.length; i++) {
+                curObj = objs[i];
+                objLabel = curObj.get(this.labelAttribute);
+                objValue = curObj.getGuid();
+                tempOption = dojoConstruct.toDom("<option value='" + objValue + "'>" + objLabel + "</option>");
+                this.selectNode.appendChild(tempOption);
+            }
+        },
+        _selectionChanged: function() {
+            this._contextObj.set(this._referenceSetToChange, this._getSelectValues(this.selectNode));
+            if (this.onChangemf) {
+                mx.data.action({
+                    params: {
+                        applyto: "selection",
+                        actionname: this.onChangemf,
+                        guids: [this._contextObj.getGuid()]
+                    },
+                    origin: this.mxform,
+                    callback: function(obj) {},
+                    error: function(error) {
+                        alert(error.message);
+                    }
+                });
+            }
+        },
+        _getSelectValues: function(select) {
+            var result = [];
+            var options = select && select.options;
+            var opt;
 
-			for (var i=0, iLen=options.length; i<iLen; i++) {
-			  opt = options[i];
+            for (var i = 0, iLen = options.length; i < iLen; i++) {
+                opt = options[i];
 
-			  if (opt.selected) {
-				result.push(opt.value || opt.text);
-			  }
-			}
-			return result;
-		},
+                if (opt.selected) {
+                    result.push(opt.value || opt.text);
+                }
+            }
+            return result;
+        },
 
-		_unsubscribe: function () {
-	      if (this.attrHandle) {
-	        mx.data.unsubscribe(this.attrHandle);
-	      }
-		  //if (this.objHandle) {
-		//	mx.data.unsubscribe(this.objHandle);
-		 // }
-	    },
-	    _resetSubscriptions : function () {
-			if (this._contextObj && this._referenceSetToListen) {
-				this._unsubscribe();
-				this.attrHandle = this.subscribe({
-				  guid : this._contextObj.getGuid(),
-				  attr : this._referenceSetToListen,
-				  callback : dojoLang.hitch(this, function (guid, attr, attrValue) {
-				    this._updateRendering();
-				  })
-				});
-			}
-	    },
+        _unsubscribe: function() {
+            if (this.attrHandle) {
+                mx.data.unsubscribe(this.attrHandle);
+            }
+            //if (this.objHandle) {
+            //	mx.data.unsubscribe(this.objHandle);
+            // }
+        },
+        _resetSubscriptions: function() {
+            if (this._contextObj && this._referenceSetToListen) {
+                this._unsubscribe();
+                this.attrHandle = this.subscribe({
+                    guid: this._contextObj.getGuid(),
+                    attr: this._referenceSetToListen,
+                    callback: dojoLang.hitch(this, function(guid, attr, attrValue) {
+                        this._updateRendering();
+                    })
+                });
+            }
+        },
 
     });
 });
